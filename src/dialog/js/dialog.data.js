39476bd12f8397cf97dbee8f700db7c01953af37
zdhxiong <zdhxiong@gmail.com>
2016-09-14 11:44:21 +0800

    修改 data 属性调用方式

--- src/dialog/js/dialog.data.js
+++ src/dialog/js/dialog.data.js
@@ -7,44 +7,41 @@ $.ready(function () {
   });
 
   // 方法按钮
-  $.each( $.queryAll('[data-md-dialog-method]'), function (index, btn) {
-    var target, inst, method, trigger;
+  var methods = ['open', 'close'];
+  $.each(methods, function(i, method){
+    $.each($.queryAll('[data-md-dialog-' + method + ']'), function(index, btn){
+      var target, inst, trigger;
 
-    // target
-    var selector = btn.getAttribute('data-md-dialog-target');
-    if(!selector){
-      var parent = $.parents(btn, '.md-dialog')[0];
-      if(parent){
-        // 如果操作按钮位于对话框中，且未指定对话框id，则默认操作该按钮所在的对话框
-        target = parent;
-      }else{
-        selector = '.md-dialog';
+      // target
+      var selector = btn.getAttribute('data-md-dialog-' + method);
+      if(!selector){
+        var parent = $.parents(btn, '.md-dialog')[0];
+        if(parent){
+          // 如果操作按钮位于对话框中，且未指定对话框id，则默认操作该按钮所在的对话框
+          target = parent;
+        }else{
+          selector = '.md-dialog';
+        }
       }
-    }
-    if(!target){
-      target = $.query(selector);
-    }
-    if(!target){
-      return;
-    }
+      if(!target){
+        target = $.query(selector);
+      }
+      if(!target){
+        return;
+      }
 
-    // inst
-    inst = $.getData(target, 'dialog.mdui');
+      // inst
+      inst = $.getData(target, 'dialog.mdui');
 
-    // method
-    method = btn.getAttribute('data-md-dialog-method');
-    if(typeof inst[method] !== 'function'){
-      method = 'open';
-    }
+      // trigger
+      trigger = btn.getAttribute('data-md-dialog-trigger');
+      if(!trigger){
+        trigger = 'click';
+      }
 
-    // trigger
-    trigger = btn.getAttribute('data-md-dialog-trigger');
-    if(!trigger){
-      trigger = 'click';
-    }
-    $.on(btn, trigger, function () {
-      inst[method]();
-    })
-
+      $.on(btn, trigger, function () {
+        inst[method]();
+      })
+    });
   });
 });
--- src/drawer/js/drawer.data.js
+++ src/drawer/js/drawer.data.js
@@ -6,36 +6,41 @@ $.ready(function () {
     $.setData(target, 'drawer.mdui', inst);
   });
 
-  // 操作按钮
-  $.each( $.queryAll('[data-md-drawer-method]'), function (index, btn) {
-    var target, inst, method, trigger;
+  // 方法按钮
+  var methods = ['open', 'close', 'toggle'];
+  $.each(methods, function(i, method){
+    $.each($.queryAll('[data-md-drawer-' + method + ']'), function(index, btn){
+      var target, inst, trigger;
 
-    // target
-    var selector = btn.getAttribute('data-md-drawer-target');
-    if (!selector) {
-      selector = '.md-drawer';
-    }
-    target = $.query(selector);
-    if (!target) {
-      return;
-    }
+      // target
+      var selector = btn.getAttribute('data-md-drawer-' + method);
+      if(!selector){
+        var parent = $.parents(btn, '.md-drawer')[0];
+        if(parent){
+          target = parent;
+        }else{
+          selector = '.md-drawer';
+        }
+      }
+      if(!target){
+        target = $.query(selector);
+      }
+      if(!target){
+        return;
+      }
 
-    // inst
-    inst = $.getData(target, 'drawer.mdui');
+      // inst
+      inst = $.getData(target, 'drawer.mdui');
 
-    // method
-    method = btn.getAttribute('data-md-drawer-method');
-    if(typeof inst[method] !== 'function'){
-      method = 'toggle';
-    }
+      // trigger
+      trigger = btn.getAttribute('data-md-drawer-trigger');
+      if(!trigger){
+        trigger = 'click';
+      }
 
-    // trigger
-    trigger = btn.getAttribute('data-md-drawer-trigger');
-    if(!trigger){
-      trigger = 'click';
-    }
-    $.on(btn, trigger, function () {
-      inst[method]();
+      $.on(btn, trigger, function () {
+        inst[method]();
+      });
     });
   });
 });

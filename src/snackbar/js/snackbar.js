d0332210e575b025c866fbec0d9800f1da68f2db
zdhxiong <zdhxiong@gmail.com>
2016-09-28 01:47:56 +0800

    Snackbar

--- src/snackbar/js/snackbar.js
+++ src/snackbar/js/snackbar.js
@@ -4,20 +4,26 @@
  */
 (function(){
 
-  // 当前正在显示的 snackbar
+  /**
+   * 当前打开着的 Snackbar
+   */
   var current;
 
+  /**
+   * 对列名
+   * @type {string}
+   */
+  var queueName = '__md_snackbar';
+
   var DEFAULT = {
-    massage: '',            // 文本内容
-    hold: 4000,             // 多长时间自动隐藏
-    minHold: 400,           // 至少停留多长时间
-    button: {
-      text: 'dismiss',      // 按钮的文本
-      color: 'blue',        // 按钮的颜色
-      close: true,          // 点击按钮后是否关闭
-      onClick: function(){} // 点击按钮的回调函数
-    },
-    onClose: function(){}   // 关闭的回掉函数
+    massage: '',                    // 文本内容
+    hold: 4000,                     // 多长时间自动隐藏
+    minHold: 400,                   // 至少停留多长时间才自动关闭
+    buttonText: 'dismiss',          // 按钮的文本
+    buttonColor: '#90CAF9',         // 按钮的颜色
+    closeOnButtonClick: true,       // 点击按钮时关闭
+    onButtonClick: function(){},    // 点击按钮的回调
+    onClose: function(){}           // 关闭后的回调
   };
 
   /**
@@ -28,17 +34,46 @@
   function Snackbar(opts){
     var inst = this;
 
-    var buttonOpts = $.extend(DEFAULT.button, (opts.button || {}));
     inst.options = $.extend(DEFAULT, (opts || {}));
 
+    // message 参数必须
+    if(!inst.options.message){
+      return;
+    }
+
+    inst.state = 'closed';
+
+    // 按钮颜色
+    var buttonColorStyle = '', buttonColorClass = '';
+    if(inst.options.buttonColor.indexOf('#') === 0 || inst.options.buttonColor.indexOf('rgb') === 0) {
+      buttonColorStyle = 'style="color:' + inst.options.buttonColor + '"';
+    }else if(inst.options.buttonColor !== ''){
+      buttonColorClass = 'md-text-color-' + inst.options.buttonColor;
+    }
+
     // 添加 HTML
-    var tpl = '<div class="md-snackbar"><div class="md-snackbar-text">' + inst.options.message + '</div><a href="javascript:void(0)" class="md-snackbar-action md-btn md-ripple md-ripple-white">' + inst.options.button.text + '</a></div>';
+    var tpl =
+      '<div class="md-snackbar ' + (mdui.screen.mdUp() ? 'md-snackbar-desktop' : 'md-snackbar-mobile') + '">' +
+        '<div class="md-snackbar-text">' + inst.options.message + '</div>' +
+        (inst.options.buttonText ? ('<a href="javascript:void(0)" class="md-snackbar-action md-btn md-ripple md-ripple-white ' + buttonColorClass + '" ' + buttonColorStyle + '>' + inst.options.buttonText + '</a>') : '') +
+      '</div>';
     inst.snackbar = $.dom(tpl)[0];
     document.body.appendChild(inst.snackbar);
 
-    // 设置样式
+    // 设置位置
     inst.snackbar.style['transform'] = 'translateY(' + inst.snackbar.clientHeight + 'px)';
     inst.snackbar.style['left'] = (document.body.clientWidth - inst.snackbar.clientWidth) / 2 + 'px';
+    $.getStyle(inst.snackbar, 'transform');
+    inst.snackbar.style['-webkit-transition'] = '-webkit-transform 0.3s cubic-bezier(0.4, 0, 0.2, 1)';
+    inst.snackbar.style['transition'] = 'transform 0.3s cubic-bezier(0.4, 0, 0.2, 1)';
+
+    // 有按钮时绑定事件
+    var action = $.query('.md-snackbar-action', inst.snackbar);
+    if(inst.options.buttonText){
+      $.on(action, 'click', function(){
+        inst.close();
+      });
+    }
   }
 
   /**
@@ -47,11 +82,25 @@
   Snackbar.prototype.open = function(){
     var inst = this;
 
-    setTimeout(function(){
-      inst.snackbar.style['-webkit-transition'] = '-webkit-transform 0.3s cubic-bezier(0.4, 0, 0.2, 1)';
-      inst.snackbar.style['transition'] = 'transform 0.3s cubic-bezier(0.4, 0, 0.2, 1)';
-      inst.snackbar.style['transform'] = 'translateY(0)';
-    }, 0);
+    // 如果当前有正在显示的 Snackbar，则先加入队列，等旧 Snackbar 关闭后再打开
+    if(current){
+      $.queue(queueName, function(){
+        inst.open();
+      });
+      return;
+    }
+
+    current = inst;
+
+    // 开始打开
+    inst.snackbar.style['transform'] = 'translateY(0)';
+
+    // 动画结束后开始计时，hold 时间到后关闭
+    $.transitionEnd(inst.snackbar, function(){
+      inst.timeoutId = setTimeout(function(){
+        inst.close();
+      }, inst.options.hold);
+    });
   };
 
   /**
@@ -61,6 +110,15 @@
     var inst = this;
 
     inst.snackbar.style['transform'] = 'translateY(' + inst.snackbar.clientHeight + 'px)';
+
+  };
+
+  /**
+   * 销毁 Snackbar
+   */
+  Snackbar.prototype.destroy = function(){
+    var inst = this;
+
 
   };
 
@@ -85,8 +143,7 @@
     }
 
     var opts = {
-      button: false,
-      onClose: function(){return false;}
+      buttonText: false
     };
 
     if(typeof params.message !== 'undefined'){

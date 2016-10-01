e4a672725022bf1c50e3df061b93f199e68f8666
zdhxiong <zdhxiong@gmail.com>
2016-10-01 16:24:50 +0800

    Text field

--- src/textfield/js/textfield.js
+++ src/textfield/js/textfield.js
@@ -6,92 +6,68 @@
 (function(){
 
   var notInputs = ['checkbox', 'button', 'submit', 'range', 'radio', 'image'];
+
   var classNames = {
     field: 'md-textfield',
     focus: 'md-textfield-focus',
-    notEmpty: 'md-textfield-not-empty'
+    notEmpty: 'md-textfield-not-empty',
+    disabled: 'md-textfield-disabled',
+    invalid: 'md-textfield-invalid'
   };
 
   /**
-   * 表单验证
-   *
-   * require        不能为空
-   * type="email"   验证邮箱
-   * type="url"     验证url
-   * type="number"  验证数值
-   * min            数值控件的最小值
-   * max            数值控件的最大值
-   * step           数值空间的 step
-   * pattern        正则表达式验证
-   * maxLength      字符串长度
-   *
-   *
-   *
-   * @param target
-   */
-  var validate = function(target){
-  };
-
-  /**
-   * 聚焦在输入框时
-   * @param e
-   */
-  var focusEvent = function(e){
-    var target = e.target;
-    var type = target.getAttribute('type');
-    if(notInputs.indexOf(type) >= 0){
-      return;
-    }
-
-    var textField = $.parents(target, '.' + classNames.field)[0];
-    textField.classList.add(classNames.focus);
-  };
-
-  /**
-   * 输入框失去焦点时
-   * @param e
-   */
-  var blurEvent = function(e){
-    var target = e.target;
-    var value = target.value;
-    var type = target.getAttribute('type');
-    if(notInputs.indexOf(type) >= 0){
-      return;
-    }
-
-    var textField = $.parents(target, '.' + classNames.field)[0];
-    textField.classList.remove(classNames.focus);
-
-    if(value && value.trim() !== ''){
-      textField.classList.add(classNames.notEmpty);
-    }else{
-      textField.classList.remove(classNames.notEmpty);
-    }
-  };
-
-  /**
-   * 输入框的值变化时
+   * 输入框事件
    * @param e
    */
   var inputEvent = function(e){
-    var target = e.target;
-    var value = target.value;
-    var type = target.getAttribute('type');
+    var input = e.target;
+    var event = e.type;
+    var value = input.value;
+
+    var type = input.getAttribute('type');
     if(notInputs.indexOf(type) >= 0){
       return;
     }
 
-    var textField = $.parents(target, '.' + classNames.field)[0];
-    if(value && value.trim() !== ''){
-      textField.classList.add(classNames.notEmpty);
+    var textField = $.parents(input, '.' + classNames.field)[0];
+
+    // 输入框是否聚焦
+    if(event === 'focus'){
+      textField.classList.add(classNames.focus);
+    }
+    if(event === 'blur'){
+      textField.classList.remove(classNames.focus);
+    }
+
+    // 输入框是否为空
+    if(event === 'blur' || event === 'input'){
+      if(value && value.trim() !== ''){
+        textField.classList.add(classNames.notEmpty);
+      }else{
+        textField.classList.remove(classNames.notEmpty);
+      }
+    }
+
+    // 输入框是否禁用
+    if(input.disabled){
+      textField.classList.add(classNames.disabled);
     }else{
-      textField.classList.remove(classNames.notEmpty);
+      textField.classList.remove(classNames.disabled);
+    }
+
+    // 表单验证
+    if(event === 'input' || event === 'blur'){
+      if(input.validity){
+        if(input.validity.valid){
+          textField.classList.remove(classNames.invalid);
+        }else{
+          textField.classList.add(classNames.invalid);
+        }
+      }
     }
   };
 
   // 绑定事件
   var inputSelector = '.'+ classNames.field +' input, .' + classNames.field + ' textarea';
-  $.on(document, 'input', inputSelector, inputEvent, true);
-  $.on(document, 'focus', inputSelector, focusEvent, true);
-  $.on(document, 'blur', inputSelector, blurEvent, true);
+  $.on(document, 'input focus blur', inputSelector, inputEvent, true);
 })();

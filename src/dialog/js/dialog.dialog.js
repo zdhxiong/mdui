bcea93ea47a0fc18049eb9f77f1cd7ecb748698d
zdhxiong <zdhxiong@gmail.com>
2016-10-17 14:51:04 +0800

    mdui.prompt()

--- src/dialog/js/dialog.dialog.js
+++ src/dialog/js/dialog.dialog.js
@@ -10,16 +10,27 @@ mdui.dialog = function (options) {
    * 默认参数
    */
   var DEFAULT = {
-    title: '',              // 标题
-    content: '',            // 文本
-    buttons: [],            // 按钮
-    stackedButtons: false,  // 垂直排列按钮
-    cssClass: '',           // 在 Dialog 上添加的 CSS 类
-    history: true,          // 监听 hashchange 事件
-    overlay: true,          // 是否显示遮罩
-    modal: false,           // 是否模态化提示框
-    closeOnEsc: true,       // 按下 esc 时关闭提示框
-    destroyOnClosed: true,  // 关闭后销毁
+    title: '',                // 标题
+    content: '',              // 文本
+    buttons: [],              // 按钮
+    stackedButtons: false,    // 垂直排列按钮
+    cssClass: '',             // 在 Dialog 上添加的 CSS 类
+    history: true,            // 监听 hashchange 事件
+    overlay: true,            // 是否显示遮罩
+    modal: false,             // 是否模态化提示框
+    closeOnEsc: true,         // 按下 esc 时关闭提示框
+    destroyOnClosed: true,    // 关闭后销毁
+    onOpen: function () {     // 打开动画开始时的回调
+    },
+
+    onOpened: function () {   // 打开动画结束后的回调
+    },
+
+    onClose: function () {    // 关闭动画开始时的回调
+    },
+
+    onClosed: function () {   // 关闭动画结束时的回调
+    },
   };
 
   /**
@@ -88,6 +99,25 @@ mdui.dialog = function (options) {
           inst.close();
         }
       });
+    });
+  }
+
+  // 绑定打开关闭事件
+  if (typeof options.onOpen === 'function') {
+    $.on(inst.dialog, 'open.mdui.dialog', function () {
+      options.onOpen(inst);
+    });
+
+    $.on(inst.dialog, 'opened.mdui.dialog', function () {
+      options.onOpened(inst);
+    });
+
+    $.on(inst.dialog, 'close.mdui.dialog', function () {
+      options.onClose(inst);
+    });
+
+    $.on(inst.dialog, 'closed.mdui.dialog', function () {
+      options.onClosed(inst);
     });
   }
 
---
+++ src/dialog/js/dialog.prompt.js
@@ -0,0 +1,112 @@
+/**
+ * =============================================================================
+ * ************   mdui.prompt(label, title, onConfirm, onCancel, options)   ************
+ * ************   mdui.prompt(label, onConfirm, onCancel, options)          ************
+ * =============================================================================
+ */
+
+mdui.prompt = function (label, title, onConfirm, onCancel, options) {
+
+  // title 参数可选
+  if (typeof title === 'function') {
+    title = '';
+    onConfirm = arguments[1];
+    onCancel = arguments[2];
+    options = arguments[3];
+  }
+
+  if (typeof onConfirm === 'undefined') {
+    onConfirm = function () {};
+  }
+
+  if (typeof onCancel === 'undefined') {
+    onCancel = function () {};
+  }
+
+  if (typeof options === 'undefined') {
+    options = {};
+  }
+
+  /**
+   * 默认参数
+   */
+  var DEFAULT = {
+    confirmText: 'ok',        // 确认按钮的文本
+    cancelText: 'cancel',     // 取消按钮的文本
+    history: true,            // 监听 hashchange 事件
+    modal: false,             // 是否模态化提示框，为 false 时点击提示框外面区域关闭提示框，为 true 时不关闭
+    closeOnEsc: true,         // 按下 esc 关闭提示框
+    type: 'text',             // 输入框类型，text: 单行文本框 textarea: 多行文本框
+    maxlength: '',            // 最大输入字符数
+    defaultText: '',          // 输入框中的默认文本
+  };
+
+  options = $.extend(DEFAULT, options);
+
+  var content =
+    '<div class="md-textfield ' + (label ? 'md-textfield-floating-label' : '') + '">' +
+      (label ? '<label class="md-textfield-label">' + label + '</label>' : '') +
+      (options.type === 'text' ?
+        '<input class="md-textfield-input" type="text" ' +
+          'value="' + options.defaultText + '" ' +
+          (options.maxlength ? ('maxlength="' + options.maxlength + '"') : '') + '/>' :
+        '') +
+      (options.type === 'textarea' ?
+        '<textarea class="md-textfield-input" ' +
+          (options.maxlength ? ('maxlength="' + options.maxlength + '"') : '') + '>' +
+            options.defaultText +
+        '</textarea>' :
+        '') +
+    '</div>';
+
+  return mdui.dialog({
+    title: title,
+    content: content,
+    buttons: [
+      {
+        text: options.cancelText,
+        bold: false,
+        close: true,
+        onClick: function (inst) {
+          var value = $.query('.md-textfield-input', inst.dialog).value;
+          onCancel(value, inst);
+        },
+      },
+      {
+        text: options.confirmText,
+        bold: false,
+        close: true,
+        onClick: function (inst) {
+          var value = $.query('.md-textfield-input', inst.dialog).value;
+          onConfirm(value, inst);
+        },
+      },
+    ],
+    cssClass: 'md-dialog-prompt',
+    history: options.history,
+    modal: options.modal,
+    closeOnEsc: options.closeOnEsc,
+    onOpen: function (inst) {
+
+      // 初始化输入框
+      var input = $.query('.md-textfield-input', inst.dialog);
+      mdui.updateTextFields(input);
+
+      // 聚焦到输入框
+      input.focus();
+
+      // 如果是多行输入框，监听输入框的 input 事件，更新提示框高度
+      if (options.type === 'textarea') {
+        $.on(input, 'input', function () {
+          inst.handleUpdate();
+        });
+      }
+
+      // 有字符数限制时，加载完文本框后 DOM 会变化，需要更新提示框高度
+      if (options.maxlength) {
+        inst.handleUpdate();
+      }
+    },
+  });
+
+};

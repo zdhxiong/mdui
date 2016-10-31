e4741fc00b39d0abbeb00efca310227c97856f1f
zdhxiong <zdhxiong@gmail.com>
2016-10-31 15:24:36 +0800

    底部导航栏

--- src/bottom_nav/js/bottom_nav.js
+++ src/bottom_nav/js/bottom_nav.js
@@ -6,74 +6,20 @@
 
 (function () {
 
-  /**
-   * 底部导航栏
-   * @param selector
-   * @constructor
-   */
-  function BottomNav(selector) {
+  $.on(document, 'click', '.mdui-bottom-nav>a', function () {
     var _this = this;
+    var bottomNav = $.parent(_this, '.mdui-bottom-nav');
+    var items = $.children(bottomNav, 'a');
 
-    _this.bottomNav = $.dom(selector)[0];
-    _this.bottomNavs = $.children(_this.bottomNav, 'a');
-
-    // 含 mdui-bottom-nav-active 的元素默认激活
-    if (typeof _this.activeIndex === 'undefined') {
-      $.each(_this.bottomNavs, function (i, bottomNav) {
-        if (bottomNav.classList.contains('mdui-bottom-nav-active')) {
-          _this.activeIndex = i;
-          return false;
-        }
-      });
-    }
-
-    // 默认激活第一个
-    if (typeof _this.activeIndex === 'undefined') {
-      _this.activeIndex = 0;
-    }
-
-    _this._setActive();
-
-    // 监听点击事件
-    $.each(_this.bottomNavs, function (i, bottomNav) {
-      $.on(bottomNav, 'click', function () {
-        _this.activeIndex = i;
-        _this._setActive();
-      });
-    });
-  }
-
-  /**
-   * 设置默认激活状态的导航项
-   */
-  BottomNav.prototype._setActive = function () {
-    var _this = this;
-
-    $.each(_this.bottomNavs, function (i, bottomNav) {
-      if (i === _this.activeIndex) {
-        $.pluginEvent('change', 'bottom-nav', _this, _this.bottomNav, {
-          index: _this.activeIndex,
+    $.each(items, function (i, curItem) {
+      if (_this === curItem) {
+        $.pluginEvent('change', 'bottomNav', null, bottomNav, {
+          index: i,
         });
 
-        if (!bottomNav.classList.contains('mdui-bottom-nav-active')) {
-          bottomNav.classList.add('mdui-bottom-nav-active');
-        }
+        curItem.classList.add('mdui-bottom-nav-active');
       } else {
-        if (bottomNav.classList.contains('mdui-bottom-nav-active')) {
-          bottomNav.classList.remove('mdui-bottom-nav-active');
-        }
-      }
-    });
-  };
-
-  $.ready(function () {
-
-    // 实例化插件
-    $.each($.queryAll('.mdui-bottom-nav'), function (i, target) {
-      var inst = $.getData(target, 'mdui-bottom-nav');
-      if (!inst) {
-        inst = new BottomNav(target);
-        $.setData(target, 'mdui-bottom-nav', inst);
+        curItem.classList.remove('mdui-bottom-nav-active');
       }
     });
   });

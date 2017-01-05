6c4e041f05e002eabdb8f69e300684498b01837a
zdhxiong <zdhxiong@gmail.com>
2017-01-05 23:12:07 +0800

    build 0.1.2

--- dist/js/mdui.js
+++ dist/js/mdui.js
@@ -1,6 +1,6 @@
 /*!
- * mdui v0.1.1 (http://mdui.org)
- * Copyright 2016-2016 zdhxiong
+ * mdui v0.1.2 (http://mdui.org)
+ * Copyright 2016-2017 zdhxiong
  * Licensed under MIT
  */
 /* jshint ignore:start */
@@ -1686,88 +1686,58 @@
    *
    * Inspired by https://github.com/nolimits4web/Framework7/blob/master/src/js/fast-clicks.js
    * https://github.com/nolimits4web/Framework7/blob/master/LICENSE
+   *
+   * Inspired by https://github.com/fians/Waves
    */
 
   (function () {
 
-    var touchStartX;
-    var touchStartY;
-    var rippleWave;
-    var rippleTarget;
-    var rippleTransform;
+    var Ripple = {
 
-    /**
-     * 找到含 mdui-ripple 类的元素，如果当前元素不存在 mdui-ripple 类，则从父元素找
-     * @param el
-     * @returns {*}
-     */
-    function findRippleElement(el) {
-      var target;
-      var rippleParents;
+      /**
+       * 显示涟漪动画
+       * @param e
+       * @param element
+       */
+      show: function (e, element) {
 
-      if (el.classList.contains('mdui-ripple')) {
-        target = el;
-      } else {
-        rippleParents = $.parents(el, '.mdui-ripple');
-        if (rippleParents.length) {
-          target = rippleParents[0];
+        // 鼠标右键不产生涟漪
+        if (e.button === 2) {
+          return;
         }
-      }
 
-      if (
-        target &&
-        target.getAttribute('disabled') === null
-      ) {
-        return target;
-      } else {
-        return false;
-      }
-    }
+        // 点击位置坐标
+        var tmp;
+        if ('touches' in e && e.touches.length) {
+          tmp = e.touches[0];
+        } else {
+          tmp = e;
+        }
 
-    /**
-     * 创建涟漪动画
-     * @param x
-     * @param y
-     * @param el
-     */
-    function createRipple(x, y, el) {
-      // 计算涟漪位置
-      // ===========
-      var box = el.getBoundingClientRect();
-      var offset = $.offset(el);
-      var center;
+        var touchStartX = tmp.pageX;
+        var touchStartY = tmp.pageY;
 
-      // 涟漪始终从中心扩散的元素
-      if (
-
-        // el.classList.contains('mdui-btn-icon') ||
-        // el.classList.contains('mdui-btn-fab') ||
-        // el.classList.contains('mdui-btn-fab-mini')
-        false
-      ) {
-        center = {
-          x: box.width / 2,
-          y: box.height / 2,
+        // 涟漪位置
+        var box = element.getBoundingClientRect();
+        var offset = $.offset(element);
+        var center = {
+          x: touchStartX - offset.left,
+          y: touchStartY - offset.top,
         };
-      }
 
-      // 其他元素上涟漪从鼠标点击位置开始扩散
-      else {
-        center = {
-          x: x - offset.left,
-          y: y - offset.top,
-        };
-      }
-
-      var height = box.height;
-      var width = box.width;
-      var diameter = Math.max(
+        var height = box.height;
+        var width = box.width;
+        var diameter = Math.max(
           Math.pow((Math.pow(height, 2) + Math.pow(width, 2)), 0.5), 48
-      );
+        );
 
-      // 添加涟漪
-      // ========
-      rippleWave = $.dom('<div class="mdui-ripple-wave" style="' +
+        // 涟漪扩散动画
+        var translate =
+          'translate3d(' + (-center.x + width / 2) + 'px, ' + (-center.y + height / 2) + 'px, 0) ' +
+          'scale(1)';
+
+        // 涟漪的 DOM 结构
+        var ripple = $.dom('<div class="mdui-ripple-wave" style="' +
           'width: ' + diameter + 'px; ' +
           'height: ' + diameter + 'px; ' +
           'margin-top:-' + diameter / 2 + 'px; ' +
@@ -1775,85 +1745,167 @@
           'left:' + center.x + 'px; ' +
           'top:' + center.y + 'px;">' +
           '</div>')[0];
-      $.prepend(el, rippleWave);
-      $.relayout(rippleWave);
-      rippleTransform =
-        'translate3d(' + (-center.x + width / 2) + 'px, ' + (-center.y + height / 2) + 'px, 0) ' +
-        'scale(1)';
-      $.transform(rippleWave, rippleTransform);
-    }
+
+        // 缓存动画效果
+        $.data(ripple, {
+          translate: translate,
+        });
+
+        $.prepend(element, ripple);
+        $.relayout(ripple);
+        $.transform(ripple, translate);
+      },
+
+      /**
+       * 隐藏涟漪动画
+       * @param e
+       * @param element
+       */
+      hide: function (e, element) {
+        element = element || this;
+
+        var ripples = $.children(element, '.mdui-ripple-wave');
+
+        $.each(ripples, function (i, ripple) {
+          removeRipple(ripple);
+        });
+
+        if (mdui.support.touch) {
+          $.off(element, 'touchmove touchend touchcancel', Ripple.hide);
+        }
+
+        $.off(element, 'mousemove mouseup mouseleave', Ripple.hide);
+      },
+    };
 
     /**
-     * 移除涟漪动画
+     * 隐藏并移除涟漪
+     * @param ripple
      */
-    function removeRipple() {
-      if (!rippleWave) {
+    function removeRipple(ripple) {
+      if (!ripple || $.data(ripple, 'isRemoved')) {
         return;
       }
 
-      var toRemove = rippleWave;
+      $.data(ripple, 'isRemoved', true);
 
       var removeTimeout = setTimeout(function () {
-        $.remove(toRemove);
+        $.remove(ripple);
       }, 400);
 
-      rippleWave.classList.add('mdui-ripple-wave-fill');
-      $.transform(rippleWave, rippleTransform.replace('scale(1)', 'scale(1.01)'));
-      $.transitionEnd(rippleWave, function (e) {
+      ripple.classList.add('mdui-ripple-wave-fill');
+      var translate = $.data(ripple, 'translate');
+      $.transform(ripple, translate.replace('scale(1)', 'scale(1.01)'));
+      $.transitionEnd(ripple, function (e) {
         clearTimeout(removeTimeout);
 
-        var rippleWave = e.target;
-        rippleWave.classList.add('mdui-ripple-wave-out');
-        $.transform(rippleWave, rippleTransform.replace('scale(1)', 'scale(1.01)'));
+        var ripple = e.target;
+        ripple.classList.add('mdui-ripple-wave-out');
+        $.transform(ripple, translate.replace('scale(1)', 'scale(1.01)'));
 
         removeTimeout = setTimeout(function () {
-          $.remove(rippleWave);
+          $.remove(ripple);
         }, 700);
 
         setTimeout(function () {
-          $.transitionEnd(rippleWave, function (e) {
+          $.transitionEnd(ripple, function (e) {
             clearTimeout(removeTimeout);
             $.remove(e.target);
           });
         }, 0);
       });
-
-      rippleWave = rippleTarget = undefined;
     }
 
-    function rippleTouchStart(el) {
-      rippleTarget = findRippleElement(el);
-      if (!rippleTarget) {
-        rippleTarget = undefined;
-        return;
+    /**
+     * touch 事件后的 500ms 内禁用 mousedown 事件
+     */
+    var TouchHandler = {
+      touches: 0,
+
+      allowEvent: function (e) {
+        var allow = true;
+
+        if (e.type === 'mousedown' && TouchHandler.touches) {
+          allow = false;
+        }
+
+        return allow;
+      },
+
+      registerEvent: function (e) {
+        var eType = e.type;
+
+        if (eType === 'touchstart') {
+          TouchHandler.touches += 1;
+        } else if (['touchmove', 'touchend', 'touchcancel'].indexOf(eType) > -1) {
+          setTimeout(function () {
+            if (TouchHandler.touches) {
+              TouchHandler.touches -= 1;
+            }
+          }, 500);
+        }
+      },
+    };
+
+    /**
+     * 找到含 .mdui-ripple 类的元素
+     * @param e
+     * @returns {*}
+     */
+    function getRippleElement(e) {
+      if (TouchHandler.allowEvent(e) === false) {
+        return null;
       }
 
-      createRipple(touchStartX, touchStartY, rippleTarget);
+      var element = null;
+      var target = e.target;
+      var rippleParents;
+
+      if (target.classList.contains('mdui-ripple')) {
+        element = target;
+      } else {
+        rippleParents = $.parents(target, '.mdui-ripple');
+        if (rippleParents.length) {
+          element = rippleParents[0];
+        }
+      }
+
+      return element;
     }
 
-    function rippleTouchMove() {
-      removeRipple();
+    /**
+     * 显示涟漪，并绑定 touchend 等事件
+     * @param e
+     */
+    function showRipple(e) {
+      var element = getRippleElement(e);
+
+      if (element !== null) {
+
+        // 禁用状态的元素上不产生涟漪效果
+        if (element.disabled || element.getAttribute('disabled')) {
+          return;
+        }
+
+        TouchHandler.registerEvent(e);
+
+        Ripple.show(e, element);
+
+        if (mdui.support.touch) {
+          $.on(element, 'touchmove touchend touchcancel', Ripple.hide);
+        }
+
+        $.on(element, 'mousemove mouseup mouseleave', Ripple.hide);
+      }
     }
 
-    function rippleTouchEnd() {
-      removeRipple();
+    // 初始化绑定的事件
+    if (mdui.support.touch) {
+      $.on(document, 'touchstart', showRipple);
+      $.on(document, 'touchmove touchend touchcancel', TouchHandler.registerEvent);
     }
 
-    // 事件监听
-    // ======
-    $.on(document, mdui.touchEvents.start, '.mdui-ripple', function (e) {
-      touchStartX = mdui.support.touch ? e.targetTouches[0].pageX : e.pageX;
-      touchStartY = mdui.support.touch ? e.targetTouches[0].pageY : e.pageY;
-      rippleTouchStart(e.target);
-    });
-
-    $.on(document, mdui.touchEvents.move, '.mdui-ripple', function () {
-      rippleTouchMove();
-    });
-
-    $.on(document, mdui.touchEvents.end, '.mdui-ripple', function () {
-      rippleTouchEnd();
-    });
+    $.on(document, 'mousedown', showRipple);
   })();
 
 

d0a9d085b427c476635db53ed60b15133affd679
zdhxiong <zdhxiong@gmail.com>
2017-08-13 22:38:50 +0800

    Select 组件

---
+++ src/select/js/select.data.js
@@ -0,0 +1,16 @@
+/**
+ * =============================================================================
+ * ************   Select 下拉选择   ************
+ * =============================================================================
+ */
+
+$(function () {
+  $('[mdui-select]').each(function () {
+    var $this = $(this);
+    var inst = $this.data('mdui.select');
+    if (!inst) {
+      inst = new mdui.Select($this, parseOptions($this.attr('mdui-select')));
+      $this.data('mdui.select', inst);
+    }
+  });
+});

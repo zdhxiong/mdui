8e5a0914d9980ffe720f55fdfa85963422155b29
zdhxiong <zdhxiong@gmail.com>
2021-12-12 22:54:14 +0800

    定义 CSS 变量

--- scripts/themes/build.js
+++ scripts/themes/build.js
@@ -1,22 +1,32 @@
 const fs = require('fs');
 const path = require('path');
+const less = require('less');
+const postcss = require('postcss');
+const autoprefixer = require('autoprefixer');
 const CleanCSS = require('clean-css');
+
 const packagePath = './packages/themes';
+const srcPath = path.join(packagePath, 'src');
 
-fs.readdirSync(packagePath).forEach((item) => {
-  const filePath = path.join(packagePath, item);
+fs.readdirSync(srcPath).forEach(async (item) => {
+  const inputPath = path.join(srcPath, item, 'index.less');
 
-  if (!filePath.endsWith('less')) {
+  if (!fs.existsSync(inputPath)) {
     return;
   }
 
-  const lessInput = fs.readFileSync(filePath).toString();
-  // @替换成--；删除//开头的注释
-  const cssContent = `:root {
-    ${lessInput.replace(/@/g, '--').replace(/\/\/.*?\n/g, '')}
-  }`;
-  const cssOutput = new CleanCSS().minify(cssContent).styles;
-  const outputName = path.resolve(filePath.replace(/(less)$/, 'css'));
+  const lessInput = fs.readFileSync(inputPath).toString();
+  const renderResult = await less.render(lessInput, {
+    filename: inputPath,
+  });
+  const postcssResult = await postcss([autoprefixer]).process(
+    renderResult.css,
+    {
+      from: undefined,
+    },
+  );
+  const cssOutput = new CleanCSS().minify(postcssResult.css).styles;
+  const outputName = path.resolve(packagePath, item) + '.css';
 
   fs.writeFileSync(outputName, cssOutput);
 });

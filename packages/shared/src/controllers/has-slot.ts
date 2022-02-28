0f3b00a7f34219e3db6670306b8b9a4d77d909ea
zdhxiong <zdhxiong@gmail.com>
2022-02-28 23:07:47 +0800

    hasSlotController

---
+++ packages/shared/src/controllers/has-slot.ts
@@ -0,0 +1,67 @@
+import type { ReactiveController, ReactiveControllerHost } from 'lit';
+
+type SlotName = '[default]' | string;
+
+/**
+ * 检查指定的 slot 是否存在
+ */
+export class HasSlotController implements ReactiveController {
+  host: ReactiveControllerHost & Element;
+  slotNames: SlotName[] = [];
+
+  constructor(
+    host: ReactiveControllerHost & Element,
+    ...slotNames: SlotName[]
+  ) {
+    (this.host = host).addController(this);
+    this.slotNames = slotNames;
+    this.onSlotChange = this.onSlotChange.bind(this);
+  }
+
+  private hasDefaultSlot() {
+    // @ts-ignore
+    return [...this.host.childNodes].some((node) => {
+      if (node.nodeType === node.TEXT_NODE && node.textContent!.trim() !== '') {
+        return true;
+      }
+
+      if (node.nodeType === node.ELEMENT_NODE) {
+        const el = node as HTMLElement;
+        if (!el.hasAttribute('slot')) {
+          return true;
+        }
+      }
+
+      return false;
+    });
+  }
+
+  private hasNamedSlot(name: string) {
+    return this.host.querySelector(`:scope > [slot="${name}"]`) !== null;
+  }
+
+  public test(slotName: SlotName) {
+    return slotName === '[default]'
+      ? this.hasDefaultSlot()
+      : this.hasNamedSlot(slotName);
+  }
+
+  hostConnected() {
+    this.host.shadowRoot!.addEventListener('slotchange', this.onSlotChange);
+  }
+
+  hostDisconnected() {
+    this.host.shadowRoot!.removeEventListener('slotchange', this.onSlotChange);
+  }
+
+  private onSlotChange(event: Event) {
+    const slot = event.target as HTMLSlotElement;
+
+    if (
+      (this.slotNames.includes('[default]') && !slot.name) ||
+      (slot.name && this.slotNames.includes(slot.name))
+    ) {
+      this.host.requestUpdate();
+    }
+  }
+}

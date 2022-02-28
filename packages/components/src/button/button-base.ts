742948530d186dad42f8912a8168e0ebfbcd6588
zdhxiong <zdhxiong@gmail.com>
2022-02-28 22:59:21 +0800

    mixin 类文件夹不含 mixin

--- packages/components/src/button/button-base.ts
+++ packages/components/src/button/button-base.ts
@@ -1,7 +1,7 @@
 import { LitElement, CSSResultGroup } from 'lit';
 import { query } from 'lit/decorators/query.js';
-import { AnchorMixin } from '@mdui/shared/mixins/anchor-mixin.js';
-import { ButtonMixin } from '@mdui/shared/mixins/button-mixin.js';
+import { AnchorMixin } from '@mdui/shared/mixins/anchor.js';
+import { ButtonMixin } from '@mdui/shared/mixins/button.js';
 import { RippleMixin } from '../ripple/ripple-mixin.js';
 import { Ripple } from '../ripple/index.js';
 import { style } from './button-base-style.js';

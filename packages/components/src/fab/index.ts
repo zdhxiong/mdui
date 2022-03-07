e788a6e94037a9a6b0941c22734de5c4558d895d
zdhxiong <zdhxiong@gmail.com>
2022-03-08 01:15:38 +0800

    fab 样式及动画优化

--- packages/components/src/fab/index.ts
+++ packages/components/src/fab/index.ts
@@ -2,6 +2,8 @@ import { html, TemplateResult, CSSResultGroup } from 'lit';
 import { customElement } from 'lit/decorators/custom-element.js';
 import { property } from 'lit/decorators/property.js';
 import { queryAll } from 'lit/decorators/query-all.js';
+import { watch } from '@mdui/shared/decorators/watch.js';
+import { HasSlotController } from '@mdui/shared/controllers/has-slot.js';
 import { ButtonBase } from '../button/button-base.js';
 import type { MaterialIconsName } from '../icon.js';
 import { style } from './style.js';
@@ -25,6 +27,8 @@ export class Fab extends ButtonBase {
   @queryAll('.button')
   protected focusProxiedElements!: HTMLElement[];
 
+  private readonly hasSlotController = new HasSlotController(this, 'icon');
+
   @property({ type: Boolean, reflect: true })
   public loading = false;
 
@@ -39,6 +43,36 @@ export class Fab extends ButtonBase {
 
   @property({ reflect: true })
   public tooltip!: string;
+
+  @property({ type: Boolean, reflect: true })
+  public extended = false;
+
+  /**
+   * extended 变更时，设置动画
+   */
+  @watch('extended')
+  private async onExtendedChange() {
+    const hasUpdated = this.hasUpdated;
+
+    if (!this.extended) {
+      this.style.width = '';
+    } else {
+      this.style.width = `${this.scrollWidth}px`;
+    }
+
+    await this.updateComplete;
+
+    if (this.extended && !hasUpdated) {
+      await new Promise((r) => setTimeout(r, 0));
+      this.style.width = `${this.scrollWidth}px`;
+    }
+
+    if (!hasUpdated) {
+      // 延迟设置动画，避免首次渲染时也执行动画
+      await new Promise((r) => setTimeout(r, 0));
+      this.style.transitionProperty = 'width';
+    }
+  }
 
   protected renderLabel(): TemplateResult {
     return html`<span part="label" class="label"><slot></slot></span>`;
@@ -56,16 +90,18 @@ export class Fab extends ButtonBase {
 
   protected override render(): TemplateResult {
     const { disabled, href } = this;
+    const hasIconSlot = this.hasSlotController.test('icon');
+    const className = `button ${this.icon || hasIconSlot ? 'has-icon' : ''}`;
 
     return html`<mdui-ripple></mdui-ripple>${href
         ? disabled
-          ? html`<span class="button">${this.renderInner()}</span>`
+          ? html`<span class=${className}>${this.renderInner()}</span>`
           : this.renderAnchor({
-              className: 'button',
+              className,
               content: this.renderInner(),
             })
         : this.renderButton({
-            className: 'button',
+            className,
             content: this.renderInner(),
           })}`;
   }

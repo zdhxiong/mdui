a1fc3b040abad44f8e96daaa4423c8c7003de730
zdhxiong <zdhxiong@gmail.com>
2022-03-07 22:35:59 +0800

    card 支持 href、clickable 属性

--- packages/components/src/card/index.ts
+++ packages/components/src/card/index.ts
@@ -3,28 +3,57 @@ import { customElement } from 'lit/decorators/custom-element.js';
 import { property } from 'lit/decorators/property.js';
 import { query } from 'lit/decorators/query.js';
 import { componentStyle } from '@mdui/shared/lit-styles/component-style.js';
+import { AnchorMixin } from '@mdui/shared/mixins/anchor.js';
+import { FocusableMixin } from '@mdui/shared/mixins/focusable.js';
+import { queryAll } from 'lit/decorators/query-all.js';
+import { when } from 'lit/directives/when.js';
 import { RippleMixin } from '../ripple/ripple-mixin.js';
 import { Ripple } from '../ripple/index.js';
 import { style } from './style.js';
 
 /**
  * @event click - 点击时触发
+ * @event focus - 获得焦点时触发
+ * @event blur - 失去焦点时触发
  */
 @customElement('mdui-card')
-export class Card extends RippleMixin(LitElement) {
+export class Card extends AnchorMixin(RippleMixin(FocusableMixin(LitElement))) {
   static override styles: CSSResultGroup = [componentStyle, style];
 
-  @query('mdui-ripple', true)
+  protected get disabled() {
+    return !this.href && !this.clickable;
+  }
+
+  @queryAll('.link')
+  protected focusProxiedElements!: HTMLElement[];
+
+  @query('mdui-ripple')
   protected rippleElement!: Ripple;
 
+  /**
+   * 卡片形状
+   */
   @property({ reflect: true })
   public variant: 'elevated' | 'filled' | 'outlined' = 'elevated';
 
+  /**
+   * 是否可点击，默认为 false。为 true 时，会添加鼠标悬浮效果、及点击涟漪效果
+   */
   @property({ type: Boolean, reflect: true })
-  public disabled = false;
+  public clickable = false;
 
   protected override render(): TemplateResult {
-    return html`<mdui-ripple></mdui-ripple><slot></slot>`;
+    const { href, clickable } = this;
+
+    if (href) {
+      return html`<mdui-ripple></mdui-ripple>${this.renderAnchor({
+          className: 'link',
+          content: html`<slot></slot>`,
+        })}`;
+    }
+
+    return html`${when(clickable, () => html`<mdui-ripple></mdui-ripple>`)}<slot
+      ></slot>`;
   }
 }
 

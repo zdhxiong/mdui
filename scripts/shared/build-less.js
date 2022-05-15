b9a4cce2f785681120613444f22797eebe6e67c0
zdhxiong <zdhxiong@gmail.com>
2022-05-15 13:39:37 +0800

    全局样式优化

--- scripts/shared/build-less.js
+++ scripts/shared/build-less.js
@@ -1,13 +1,6 @@
 const fs = require('fs');
-const child_process = require('child_process');
-
-child_process.spawnSync('cp', [
-  '-r',
-  './packages/shared/src/variables',
-  './packages/shared/variables',
-]);
 
 fs.copyFileSync(
-  './packages/shared/src/variables.less',
-  './packages/shared/variables.less',
+  './packages/shared/src/mixin.less',
+  './packages/shared/mixin.less',
 );

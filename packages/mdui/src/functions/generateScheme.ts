25d20d6cf51863333601e366f37bcc47154cc59f
bre 97 <by.sgatn3@Gmail.com>
2024-03-17 14:15:13 +0800

    feat: generateScheme.ts

---
+++ packages/mdui/src/functions/generateScheme.ts
@@ -0,0 +1,4 @@
+export * from './utils/contrast.js';
+export { type TMaterialColors } from './utils/materialColors.js';
+export * from './utils/schemeGenerator.js';
+export * from './utils/variant.js';
